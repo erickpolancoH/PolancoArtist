5a03d02fa5046f694e09debf72cb1696775e2a42
erickpolancoH <polancof292cd50@outlook.com>
2021-10-03 19:22:37 -0400

    se modeifico cosas

--- polancoartist.js
+++ polancoartist.js
@@ -1,1 +1,23 @@
+var video = videojs( 'my-video');
 
+    document.getElementById('action').onclick = function(){
+        video.play();
+    }
+
+    var video = videojs( 'mivideo');
+
+    document.getElementById('action').onclick = function(){
+        video.play();
+    }
+
+    var video = videojs( 'mivideo2');
+
+    document.getElementById('action').onclick = function(){
+        video.play();
+    }
+
+    var video = videojs( 'mivideo3');
+
+    document.getElementById('action').onclick = function(){
+        video.play();
+    }
